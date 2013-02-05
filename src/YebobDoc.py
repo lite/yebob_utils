d6976e117f227f1064aab7e5beb6b5655b1dbc5b
dli <litekok@gmail.com>
2013-02-05 17:21:04 +0800

    remove html tags

--- src/YebobDoc.py
+++ src/YebobDoc.py
@@ -5,8 +5,8 @@ import re
 
 class YebobDoc:
     def from_html(self, html):
-        text = re.sub("<.+>\n", "", html)
-        text = re.sub("</.+>\n", "", text)
+        text = re.sub("<.+>\s*", "", html)
+        text = re.sub("</.+>\s*", "", text)
         text = re.sub('(<br/?>\s*)+', '\n', text)
         text = re.sub('&nbsp;', ' ', text)
         return text
