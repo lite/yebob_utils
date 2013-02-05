ffce2cf46929cb015ed3a46b050691bfef0bfc60
dli <litekok@gmail.com>
2013-02-05 16:57:34 +0800

    use re.sub, html2text has memory issue

--- src/YebobDoc.py
+++ src/YebobDoc.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import html2text
 import re
 
 class YebobDoc:
-
-    def __init__(self):
-        self.h = html2text.HTML2Text()
-        self.h.ignore_links = True
-
     def from_html(self, html):
-        text = self.h.handle(html).strip()
-        strs = re.compile("\n+").split(text)
-        return "\n".join(strs)
+        text = re.sub("<.+>\n", "", html)
+        text = re.sub("</.+>\n", "", text)
+        text = re.sub('(<br/?>\s*)+', '\n', text)
+        text = re.sub('&nbsp;', ' ', text)
+        return text
