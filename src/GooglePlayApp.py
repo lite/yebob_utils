6a4af9023dcef22671347e1d0f03b285334947e5
lite <litekok@gmail.com>
2013-08-06 23:27:55 +0800

    google app store upgraded

--- src/GooglePlayApp.py
+++ src/GooglePlayApp.py
@@ -30,22 +30,28 @@ class GooglePlayApp:
         data = res.get_data() 
         soup = BeautifulSoup(data, "html5lib", from_encoding="utf-8")
         info = AppInfo()
-        info.name = soup.find('h1', attrs={"class" : "doc-banner-title"}).text
+        # info.name = soup.find('h1', attrs={"class" : "doc-banner-title"}).text
+        info.name = soup.find('div', attrs={"class" : "document-title"}).text
         info.category = ""
         info.version = ""
         info.size = ""
         info.updated = ""
         info.price = ""
         info.os = "Android"
-        info.developer = soup.find('a', attrs={"class" : "doc-header-link"}).text
+        # info.developer = soup.find('a', attrs={"class" : "doc-header-link"}).text
+        info.developer = soup.find('a', attrs={"class" : "document-subtitle"}).text
         info.language = ""
-        desc = soup.find(id="doc-original-text").prettify()
+        # desc = soup.find(id="doc-original-text").prettify()
+        desc = soup.find('div', attrs={"class" : "show-more-content"}).prettify()
+        
         info.description = desc
 
-        artwork = soup.find('div', attrs={"class" : "doc-banner-icon"}).img["src"]
+        # artwork = soup.find('div', attrs={"class" : "doc-banner-icon"}).img["src"]
+        artwork = soup.find('div', attrs={"class" : "cover-container"}).img["src"]
         info.artwork = artwork
 
-        div_images = soup.find('div', attrs={"class" : "screenshot-carousel-content-container"})
+        # div_images = soup.find('div', attrs={"class" : "screenshot-carousel-content-container"})
+        div_images = soup.find('div', attrs={"class" : "thumbnails"})
         images = div_images.find_all('img')
         info.images = [img["src"] for img in images]
 
